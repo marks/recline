marks/recline

commit cdd31faafa13c6660894556d2826219703f9b146

[#427] tackling breaking style changes
beginning with multi view demo (not done yet)

src/view.multiview.js
@@ -103,7 +103,7 @@ my.MultiView = Backbone.View.extend({
       <div class="navigation"> \
         <div class="btn-group" data-toggle="buttons-radio"> \
         {{#views}} \
-        <a href="#{{id}}" data-view="{{id}}" class="btn">{{label}}</a> \
+        <button href="#{{id}}" data-view="{{id}}" class="btn btn-default">{{label}}</button> \
         {{/views}} \
         </div> \
       </div> \
@@ -113,7 +113,7 @@ my.MultiView = Backbone.View.extend({
       <div class="menu-right"> \
         <div class="btn-group" data-toggle="buttons-checkbox"> \
           {{#sidebarViews}} \
-          <a href="#" data-action="{{id}}" class="btn">{{label}}</a> \
+          <button href="#" data-action="{{id}}" class="btn btn-default">{{label}}</button> \
           {{/sidebarViews}} \
         </div> \
       </div> \

css/slickgrid.css
@@ -169,7 +169,7 @@ classes should alter those!
   font-size: 12px;
   padding: 2px;
   width: 22px;
-  height: 14px;
+  height: 16px;
   line-height: 13px;
 }
 

src/view.slickgrid.js
@@ -12,7 +12,7 @@ this.recline.View = this.recline.View || {};
   my.GridControl= Backbone.View.extend({
     className: "recline-row-add",
     // Template for row edit menu , change it if you don't love
-    template: '<h1><a href="#" class="recline-row-add btn">Add row</a></h1>',
+    template: '<h1><button href="#" class="recline-row-add btn btn-default">Add row</button></h1>',
     
     initialize: function(options){
       var self = this;
@@ -73,7 +73,7 @@ my.SlickGrid = Backbone.View.extend({
 	
     // Template for row delete menu , change it if you don't love 
     this.templates = {
-	    "deleterow" : '<a href="#" class="recline-row-delete btn" title="Delete row">X</a>'
+	    "deleterow" : '<button href="#" class="recline-row-delete btn" title="Delete row">X</button">'
     };
 
     _.bindAll(this, 'render', 'onRecordChanged');

src/widget.pager.js
@@ -10,7 +10,7 @@ my.Pager = Backbone.View.extend({
   className: 'recline-pager', 
   template: ' \
     <div class="pagination"> \
-      <ul> \
+      <ul class="pagination"> \
         <li class="prev action-pagination-update"><a href="">&laquo;</a></li> \
         <li class="active"><label for="from">From</label><a><input name="from" type="text" value="{{from}}" /> &ndash; <label for="to">To</label><input name="to" type="text" value="{{to}}" /> </a></li> \
         <li class="next action-pagination-update"><a href="">&raquo;</a></li> \

src/widget.queryeditor.js
@@ -14,7 +14,7 @@ my.QueryEditor = Backbone.View.extend({
         <span class="add-on"><i class="icon-search"></i></span> \
         <label>Search</label><input type="text" name="q" value="{{q}}" class="span2" placeholder="Search data ..." class="search-query" /> \
       </div> \
-      <button type="submit" class="btn">Go &raquo;</button> \
+      <button type="submit" class="btn btn-default">Go &raquo;</button> \
     </form> \
   ',
 

commit fc3cd5504d9071ef8f21fb726a305c1184f2b302

[#427] fix broken events introduced by cdd31faafa13c6660894556d2826219703f9b146

src/view.multiview.js
@@ -124,8 +124,8 @@ my.MultiView = Backbone.View.extend({
   </div> \
   ',
   events: {
-    'click .menu-right a': '_onMenuClick',
-    'click .navigation a': '_onSwitchView'
+    'click .menu-right button': '_onMenuClick',
+    'click .navigation button': '_onSwitchView'
   },
 
   initialize: function(options) {